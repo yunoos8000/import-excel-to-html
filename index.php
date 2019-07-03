9e7ad3b6415be22545e9659593372333dd9cf153
Yunoos Parvez <yunoos8000@gmail.com>
2019-07-04 00:04:14 +0530

    Add All files

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+	</head>
+	<body>
+<h2>Excel File Uploader</h2>
+
+<form  action="excel-upload.php"  method="POST" enctype="multipart/form-data" >
+	
+	<!--<label class="form-label span3" for="file">File</label>-->
+	<input type="file" name="file" id="file" >
+	
+	<input type="submit"  value="Submit" name="submit1">
+</form>
+	</body>
+	
+</html>
